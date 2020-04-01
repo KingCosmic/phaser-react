04ba7e781a3510d6e5b03fc565fff481fb0b5a09
KingCosmic <KingCosmicDe@gmail.com>
2020-04-01 17:08:47 -0500

    added support for a few options.

---
+++ src/plugin.tsx
@@ -0,0 +1,51 @@
+import { Plugins } from 'phaser';
+import { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+
+import Renderer from './renderer';
+
+import manager from './manager';
+
+type PluginOptions = {
+  // the parent element's id to render react dom into
+  // if not set will wrap the game canvas with a div and render to that.
+  parent: string,
+  // if we shouldn't inject our own react dom
+  // useful if you already have react
+  dontInjectReact: boolean
+}
+
+class ReactUI extends Plugins.BasePlugin {
+  constructor(pluginManager: Plugins.PluginManager) {
+    super(pluginManager);
+
+    //  Register our new Game Object type
+    pluginManager.registerGameObject('reactDom', this.createReactDom);
+  }
+
+  init(options?: PluginOptions) {
+
+    if (options.dontInjectReact) return;
+    if (!options.parent) {
+      let parent = document.createElement('div');
+      parent.appendChild(this.game.canvas);
+
+      ReactDOM.render(
+        <Renderer />,
+        parent
+      );
+    } else {
+      ReactDOM.render(
+        <Renderer />,
+        document.getElementById(options.parent)
+      )
+    }
+  }
+
+  createReactDom(component: ReactNode, props: Object) {
+    return manager.addUI(component, props);
+  }
+
+}
+
+export default ReactUI;
