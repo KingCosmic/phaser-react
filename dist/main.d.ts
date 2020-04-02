fd33631f02b3299c939d7aad7f24338da0813ad0
KingCosmic <KingCosmicDev@gmail.com>
2020-04-02 17:40:38 -0500

    testing a type def fix

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for phaser3-react 1.0.15
+// Type definitions for phaser3-react 1.0.17
 // Project: phaser-react
 // Definitions by: Austyn Studdard <https://github.com/KingCosmic>
 
@@ -6,3 +6,22 @@ import Plugin from '../src/plugin';
 
 export { default as Renderer } from '../src/renderer'
 export default Plugin;
+
+/*~ On this line, import the module which this module adds to */
+import * as phaser from 'phaser';
+import { Component } from 'react';
+import { ComponentManager } from '../src/manager';
+
+interface GameObjectFactory {
+  reactDom(component: Component, state: Object): ComponentManager;
+}
+
+/*~ Here, declare the same module as the one you imported above */
+declare module 'phaser' {
+
+  /*~ You can also add new properties to existing interfaces from
+   *~ the original module by writing interface augmentations */
+  export interface GameObjects {
+    GameObjectFactory: GameObjectFactory;
+  }
+}
